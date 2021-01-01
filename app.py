2be023e1c7f0c0d8cfe37d26b6a98f8517336854
Shantanu Tomar <shantanu.tomar94@gmail.com>
2021-01-01 23:18:38 +0530

    Add frontend static

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import os, re, boto3
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from werkzeug.security import generate_password_hash, check_password_hash 
 from sqlalchemy.orm import validates
 from werkzeug.utils import secure_filename
+from werkzeug.routing import BaseConverter
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['FLASK_APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JWT_SECRET_KEY'] = os.environ['FLASK_JWT_SECRET_KEY']
 app.config['JWT_HEADER_TYPE'] = 'Token'
+CORS(app)
 
-api = Api(app, prefix="/api")
+api = Api(app)
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 limiter = Limiter(
@@ -33,15 +35,13 @@ class User(db.Model):
     username = db.Column(db.String(20), index=True, unique=True, nullable=False)
     password = db.Column(db.String())
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, password2):
         self.username = username
         self.password = generate_password_hash(password)
 
     @validates('username') 
     def validate_username(self, key, username):
-        if not username:
-            raise AssertionError('No username provided.')
-        
+        print(username)
         if User.query.filter(User.username == username).first():
             raise AssertionError('Username is already in use.')
         
@@ -49,30 +49,19 @@ class User(db.Model):
             raise AssertionError('Username must be between 5 and 20 characters.')
         return username
     
-    def __repr__(self):
-        return str(self.id)
-    
-    def serialize(self):
-        return {
-            'id': self.id, 
-            'username': self.username,
-        }
-
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
 
 class Signup(Resource):
     def post(self):
-        username = request.form.get('username')
-        password = request.form.get('password')
-        
+        body = request.get_json()
         try:
-            user = User(username=username, password=password)
+            user = User(**body)
             db.session.add(user)
             db.session.commit()
         except AssertionError as e:
-            return {'error': e}, 400
+            return {'error': str(e)}, 400
 
         access_token = create_access_token(identity=str(user.id))
 
@@ -80,14 +69,12 @@ class Signup(Resource):
 
 class LoginApi(Resource):
     def post(self):
-        username = request.form.get('username')
-        password = request.form.get('password')
-        
-        user = User.query.filter_by(username=username).first()
+        body = request.get_json()
+        user = User.query.filter_by(username=body.get('username')).first()
         if user is None:
             return {'error': 'User with that username does not exist.'}, 400
 
-        authorized = user.check_password(password)
+        authorized = user.check_password(body.get('password'))
         if not authorized:
             return {'error': 'Email or password invalid'}, 401
       
@@ -108,7 +95,7 @@ class UploadImage(Resource):
                 aws_secret_access_key=os.environ['MY_AWS_SECRET_ACCESS_KEY'],
             )
             filename = secure_filename(img.filename)
-            #img.save(filename)
+            img.save(filename)
             s3.upload_file(
                 Bucket=BUCKET_NAME,
                 Filename=filename,
@@ -125,9 +112,16 @@ class UploadImage(Resource):
             return {'image': url, 'name': filename}, 201
 
 
-api.add_resource(Signup, '/signup')
-api.add_resource(LoginApi, '/login')
-api.add_resource(UploadImage, '/upload')
+api.add_resource(Signup, '/api/signup')
+api.add_resource(LoginApi, '/api/login')
+api.add_resource(UploadImage, '/api/upload')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def render_frontend(path):
+    print(path)
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()

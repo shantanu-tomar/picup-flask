bd31997ef446618ae9b422cd0c913dfcadbad4f9
Shantanu Tomar <shantanu.tomar94@gmail.com>
2021-01-01 23:32:57 +0530

    Change database url var

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['FLASK_SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['FLASK_DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
